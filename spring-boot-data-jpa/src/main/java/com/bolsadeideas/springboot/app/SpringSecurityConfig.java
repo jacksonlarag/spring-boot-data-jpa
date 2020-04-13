23a85c3ee88bc7e2d5001d1e2899870f5b9eca6a
jackson <jacksonlarag@gmail.com>
2020-04-12 20:03:33 -0500

    12/04/2020

---
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -0,0 +1,86 @@
+package com.bolsadeideas.springboot.app;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.bolsadeideas.springboot.app.auth.handler.LoginSuccessHandler;
+import com.bolsadeideas.springboot.app.models.service.JpaUserDetailsService;
+
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private LoginSuccessHandler successHandler;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	/*
+	 * JDBC
+	@Autowired
+	private DataSource dataSource;
+	*/
+	
+	@Autowired
+	private JpaUserDetailsService userDetailsService;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/listar","/locale").permitAll()
+		/*.antMatchers("/ver/**").hasAnyRole("USER")*/
+		/*.antMatchers("/uploads/**").hasAnyRole("USER")*/
+		/*.antMatchers("/form/**").hasAnyRole("ADMIN")*/
+		/*.antMatchers("/eliminar/**").hasAnyRole("ADMIN")*/
+		/*.antMatchers("/factura/**").hasAnyRole("ADMIN")*/
+		.anyRequest().authenticated()
+		.and()
+			.formLogin()
+			.successHandler(successHandler)
+				.loginPage("/login")
+			.permitAll()
+		.and()
+		.logout().permitAll()
+		.and()
+		.exceptionHandling().accessDeniedPage("/error_403");
+	}
+
+
+	
+	@Autowired
+	public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception{
+		//CON JPA
+		builder.userDetailsService(userDetailsService)
+		.passwordEncoder(passwordEncoder);
+		
+		/*
+		 * JDBC
+		builder.jdbcAuthentication()
+		.dataSource(dataSource)
+		.passwordEncoder(passwordEncoder)
+		.usersByUsernameQuery("select username, password, enabled from users where username=?")
+		.authoritiesByUsernameQuery("select u.username, a.authority from authorities a inner join users u on (a.user_id = u.id) where u.username=?");
+		
+		*/
+		
+		/*
+		 * EN DURO
+		PasswordEncoder encoder = this.passwordEncoder;
+		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+		
+		builder.inMemoryAuthentication()
+		.withUser(users.username("admin").password("12345").roles("ADMIN","USER"))
+		.withUser(users.username("jackson").password("12345").roles("USER"));
+		*/
+	}
+}
